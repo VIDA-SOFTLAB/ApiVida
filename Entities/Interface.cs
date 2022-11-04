93730081d291425c8c94eac72f35cefc6c239464
srtam <amandakrdesa@gmail.com>
2022-11-04 13:56:47 -0300

    entity and controllers creation

---
+++ Entities/Interface.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiVida.Entities
+{
+    public interface Interface
+    {
+        public int userId { get; set; }
+        public string userName { get; set; }
+        public string cpf { get; set; }
+        public string rg { get; set; }
+        public string phoneNumber { get; set; }
+        public DateTime birthDate { get; set; }
+        public DateTime registerDate   { get; set; }
+        public DateTime lastRegisterUpdate { get; set; }
+        public string email { get; set; }
+    }
+}
