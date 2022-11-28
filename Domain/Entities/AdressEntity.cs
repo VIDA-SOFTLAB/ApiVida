1d83575367281eaa0e17aee8f80998010663e872
ayrous <iris_campanella@hotmail.com>
2022-11-28 10:40:55 -0300

    atualizacao endereco em ingles

--- Domain/Entities/AdressEntity.cs
+++ Domain/Entities/AdressEntity.cs
@@ -14,42 +14,42 @@ namespace Athenas.Domain
         public string Id { get; set; }
 
         [Required(ErrorMessage = ErrorBase.erro_nec)]
-        [JsonProperty(PropertyName = "cep")]
-        public string Cep { get; set; }
+        [JsonProperty(PropertyName = "zipCode")]
+        public string ZipCode { get; set; }
 
         [Required(ErrorMessage = ErrorBase.erro_nec)]
         [MinLength(4, ErrorMessage = ErrorBase.erro_min)]
         [MaxLength(31, ErrorMessage = ErrorBase.erro_max)]
-        [JsonProperty(PropertyName = "bairro")]
-        public string Bairro { get; set; }
+        [JsonProperty(PropertyName = "district")]
+        public string District { get; set; }
 
         [Required(ErrorMessage = ErrorBase.erro_nec)]
         [MinLength(4, ErrorMessage = ErrorBase.erro_min)]
         [MaxLength(128, ErrorMessage = ErrorBase.erro_max)]
-        [JsonProperty(PropertyName = "localidade")]
-        public string Localidade { get; set; }
+        [JsonProperty(PropertyName = "locality")]
+        public string Locality { get; set; }
 
         [Required(ErrorMessage = ErrorBase.erro_nec)]
         [MinLength(2, ErrorMessage = ErrorBase.erro_min)]
         [MaxLength(2, ErrorMessage = ErrorBase.erro_max)]
-        [JsonProperty(PropertyName = "uf")]
-        public string Uf { get; set; }
+        [JsonProperty(PropertyName = "fu")]
+        public string fu { get; set; }
 
         [Required(ErrorMessage = ErrorBase.erro_nec)]
         [MinLength(3, ErrorMessage = ErrorBase.erro_min)]
         [MaxLength(31, ErrorMessage = ErrorBase.erro_max)]
-        [JsonProperty(PropertyName = "logradouro")]
-        public string Logradouro { get; set; }
+        [JsonProperty(PropertyName = "publicPlace")]
+        public string PublicPlace { get; set; }
 
         [Required(ErrorMessage = ErrorBase.erro_nec)]
         [MinLength(1, ErrorMessage = ErrorBase.erro_min)]
         [MaxLength(7, ErrorMessage = ErrorBase.erro_max)]
-        [JsonProperty(PropertyName = "numero")]
-        public string Numero { get; set; }
+        [JsonProperty(PropertyName = "number")]
+        public string Number { get; set; }
 
         [MinLength(3, ErrorMessage = ErrorBase.erro_min)]
         [MaxLength(127, ErrorMessage = ErrorBase.erro_max)]
-        [JsonProperty(PropertyName = "complemento")]
-        public string Complemento { get; set; }
+        [JsonProperty(PropertyName = "adressComplement")]
+        public string AdressComplement { get; set; }
     }
 }
