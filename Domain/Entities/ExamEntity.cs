b526090aa11e3ae4318c19ba82af173b161dcd41
Mtsj8 <marcelojunior.tranche@gmail.com>
2022-12-09 20:27:21 -0300

    Entidade de exame

---
+++ Domain/Entities/ExamEntity.cs
@@ -0,0 +1,49 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using ApiVida.Domain.Errors;
+using ApiVida.Domain.Entities;
+
+namespace ApiVida.Domain.Entities
+{
+    public class ExamEntity
+    {
+
+        [Key]
+        [JsonProperty(PropertyName = "examId")]
+        public string ExamId { get; set; }
+		
+        [Required(ErrorMessage = ErrorBase.erro_camponec)]
+        [JsonProperty(PropertyName = "specialty")]
+        public string Specialty { get; set; }
+
+        //[Required(ErrorMessage = ErrorBase.erro_camponec)]
+        [JsonProperty(PropertyName = "medicalCenterId")]
+        public string MedicalCenterId { get; set; }
+
+//        [Required(ErrorMessage = ErrorBase.erro_camponec)]
+        [DataType(DataType.Date, ErrorMessage = ErrorBase.erro_for)]
+        [JsonProperty(PropertyName = "examDatetime")]
+        public DateTime ExamDatetime { get; set; }
+
+  //      [Required(ErrorMessage = ErrorBase.erro_camponec)]
+        [DataType(DataType.Date, ErrorMessage = ErrorBase.erro_for)]
+        [JsonProperty(PropertyName = "registerDate")]
+        public DateTime RegisterDate { get; set; }
+
+    //    [Required(ErrorMessage = ErrorBase.erro_camponec)]
+        [DataType(DataType.Date, ErrorMessage = ErrorBase.erro_for)]
+        [JsonProperty(PropertyName = "lastRegisterUpdate")]
+        public DateTime LastRegisterUpdate { get; set; }
+
+        [JsonProperty(PropertyName = "patientId")]
+        public string PatientId { get; set; }
+
+        [JsonProperty(PropertyName = "doctorId")]
+        public string DoctorId { get; set; }
+
+    }
+}
